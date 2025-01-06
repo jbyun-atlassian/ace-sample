0c430debf72bea5368e5bd7e18f6d311420cd61d
James Byun <jbyun@atlassian.com>
2025-01-06 16:01:59 +1100

    memory to file

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,27 @@
+function get(httpClient, url) {
+  return new Promise(function (resolve, reject) {
+    httpClient.get(
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        url,
+      },
+      function (err, response, body) {
+        if (err) {
+          reject({
+            statusCode: response.statusCode,
+            message: err,
+          });
+        } else {
+          resolve(body);
+        }
+      }
+    );
+  });
+}
+
 export default function routes(app, addon) {
     // Redirect root path to /atlassian-connect.json,
     // which will be served by atlassian-connect-express.
@@ -7,9 +31,15 @@ export default function routes(app, addon) {
 
     // This is an example route used by "generalPages" module (see atlassian-connect.json).
     // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
+    app.get('/hello-world', addon.authenticate(), async (req, res) => {
         // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
         // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
+        
+        const apiSample = `/rest/atlassian-connect/1/addons/${addonKey}`;
+        console.log("licenseApi url", licenseApi);
+        const licApiResponse = await get(httpClient, licenseApi);
+
+        
         res.render(
           'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
           {
@@ -19,6 +49,8 @@ export default function routes(app, addon) {
           }
         );
     });
+
+
 
     // Add additional route handlers here...
 }
